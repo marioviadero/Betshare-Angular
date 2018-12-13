8547eeb6713716e3f52219b6a3fd1e2d25365c5a
marioviadero <mvh87@outlook.es>
2018-12-13 12:20:17 +0100

    Servicio coge datos de APP SPRING

--- src/app/shared/usuarios.service.ts
+++ src/app/shared/usuarios.service.ts
@@ -10,9 +10,9 @@ export class UsuariosService {
   constructor(private http: HttpClient) { }
 
   listaUsuarios() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+    return this.http.get('http://localhost:8080/usuario/lista-usuarios');
   }
   getUsuario(userId){
-    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+    return this.http.get('http://localhost:8080/usuario/'+userId)
   }
 }
